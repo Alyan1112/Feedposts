e3bf44d391e60c99831c41eee293e849a4f3256c
Alyan1112 <132790646+Alyan1112@users.noreply.github.com>
2023-07-31 05:24:10 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://shorturl.at/egiot/`
+					`https://www.featureposts.com/2023/07/lena-and-adam-best-scenes-watch.html/`
 				}`,
 			},
 		};
